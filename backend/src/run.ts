a8908ec67ae712cbff62b727bef3e837a2b4e300
siddhant ota <siddhantota@protonmail.com>
2023-08-09 21:45:37 +0530

    sid route added

--- backend/src/run.ts
+++ backend/src/run.ts
@@ -8,6 +8,11 @@ const app: express.Application = express()
 app.get('/sid', (req, res) => {
     res.send('hello!!, I am the site author')
 })
+
+app.get('/test', (req, res) => {
+    res.send('hello, this is a test')
+})
+
 app.use(cors())
 app.use(express.json())
 app.use(itemRouter)
